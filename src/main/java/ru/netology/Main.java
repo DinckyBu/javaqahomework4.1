693f0d61967f36fd55031518ce14159613370f2d
Екатерина Кобелева <kobelevaekaterina09@gmail.com>
2023-04-20 20:20:25 +0300

    Changed project

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
-int expected = 150;
-        long actual = service.calculate( 5_000, true);
+        int expected = 150;
+        long actual = service.calculate(5_000, true);
         System.out.println("1. " + expected + " == ? == " + actual);
 
         expected = 50;
-        actual = service.calculate( 5_000, false);
+        actual = service.calculate(5_000, false);
         System.out.println("2. " + expected + " == ? == " + actual);
     }
 }
